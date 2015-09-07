09f46c92ccb6276ea470e69aa1dcef0bf037203e
Paul Anderson <andersonpe2@cofc.edu>
2015-09-07 15:52:19 -0400

    Added get all genes functions and static output file.

--- R/get.genes.R
+++ R/get.genes.R
@@ -4,6 +4,80 @@ library(XML)
 library(KEGGREST)
 library(RCurl)
 
+#' Downloading all genes in all WikiPathways pathways
+#'
+#' Returns the gene ids in WikiPathways for a given species
+#' For more information on WikiPathways see http://webservice.wikipathways.org/ and
+#' http://www.wikipathways.org/index.php/Help:WikiPathways_Webservice/API
+#' 
+#' @param url WikiPathways base URL for webservice calls.
+#' @param species Species for pathway analysis
+#' @return a dataframe
+#' 
+#' @examples
+#' gene.ids = get.all.genes.wiki(webg.pathways)
+#' 
+#' @export
+#' 
+get.all.genes.wiki <- function(url="http://webservice.wikipathways.org",species="Homo sapiens") {
+  # Get a list of all pathways from WikiPathways
+  XML =getURL(paste(url,"/listPathways",sep=""))    
+  # Parse the xml document
+  doc = xmlParse(XML, asText=TRUE)
+  
+  # Extract the pathway info
+  pathwayNodes = xmlElementsByTagName(xmlRoot(doc), "pathways", TRUE)
+  pathways = lapply(pathwayNodes, function(n) {
+    children = xmlChildren(n, addNames= TRUE)
+    if(xmlValue(children$species) == species) {
+      p = list()
+      p[["id"]] = xmlValue(children$id)
+      p[["name"]] = xmlValue(children$name)
+      p[["species"]] = xmlValue(children$species)
+      p[["url"]] = xmlValue(children$url)
+      return(p)
+    } else {
+      return() # Skip non-species pathways
+    }
+  })
+  
+  # Remove NULL entries (non-human pathways)
+  pathways = pathways[!sapply(pathways, is.null)]
+  #new.pathways = pathways[sapply(pathways, function(p) {
+  #  return(p$id %in% webg.pathways)
+  #})]
+  
+  # A function that downloads a GeneSet for a pathway
+  createGS = function(p) {
+    print(p[["id"]])
+    # Download the gene list (translated to Entrez ids)
+    XML = getURL(paste(url,"/getXrefList?pwId=",p[['id']],"&code=L",sep=""))
+    doc = xmlParse(XML, asText=TRUE)
+    # Find the xref nodes with an xpath query
+    resultNodes = xmlElementsByTagName(xmlRoot(doc), "xrefs", TRUE)
+    # Extract the gene ids
+    geneIds = sapply(resultNodes, xmlValue)
+    if(length(geneIds) > 0) { # Skip empty lists
+      # Create a GeneSet object
+      geneSet = GeneSet(geneIds, geneIdType=EntrezIdentifier(), 
+                        setName=paste(p[["id"]], " (", p[["name"]], ")", sep="")
+      )
+      print(geneSet)
+      return(geneSet)
+    }
+  }
+  geneSets = lapply(pathways, createGS) #Apply the createGS function on all pathways
+  geneSets = geneSets[!sapply(geneSets, is.null)] #Remove empty sets
+  all.gene.ids = c()
+  for (i in 1:length(geneSets)) {
+    all.gene.ids = c(all.gene.ids,as.vector(geneIds(geneSets[[i]])))
+  }
+  all.gene.ids = unique(all.gene.ids)
+  return(as.numeric(all.gene.ids))
+  
+}
+
+
 #' Downloading WikiPathways to access gene ids
 #'
 #' Returns the gene ids associated with select WikiPathways
@@ -148,6 +222,74 @@ get.genes.kegg <- function(webg.pathways,sleep=10) {
   genes = unique(genes)
   return(as.numeric(genes))
 
+}
+
+#' Downloading gene ids from all Kegg Pathways
+#'
+#' Returns the gene ids
+#' 
+#' @param sleep Number of seconds to sleep between Kegg requests
+#' @param species Species identifier for Kegg. Default is hsa.
+#' @return an array of gene ids
+#' 
+#' @examples
+#' gene.ids = get.all.genes.kegg()
+#' 
+#' @export
+#' 
+get.all.genes.kegg <- function(sleep=10,species="hsa") {
+  pathways.kegg.list = keggList("hsa")
+  pathways = matrix(1:(2*length(names(pathways.kegg.list))),ncol=2)
+  pathways[,2] = names(pathways.kegg.list)
+  pathways[,1] = pathways.kegg.list
+  
+  genes = c()
+  #genes = read.csv('genes.csv')
+  #genes = as.character(genes$x)
+  z = 1
+  #start = read.csv('currenti.csv')
+  #start = start$x[1]+1
+  pathways.with.no.genes = c()
+  k = 1
+  while (z <= nrow(pathways)) {
+    submissions = c()
+    for (j in z:nrow(pathways)) {
+      submissions[j-z+1] = paste(pathways[j,2],sep="")
+      if (length(submissions) == 10) # Kegg restrictions
+        break
+    }
+
+    results = keggGet(submissions)
+    for (i in 1:length(results)) {
+      if (length(results) > 0 && sum(attributes(results[[i]])$name=="GENE") > 0) {
+        if (length(results[[i]]$GENE) == 0) {
+          pathways.with.no.genes[k] = pathways[i,2]
+          k = k + 1
+          next
+        }
+        for (j in seq(1,length(results[[i]]$GENE),2)) { # Skip gene name and just go with Entrez ID that is returned
+          fields = strsplit(results[[i]]$GENE[j],";")
+          genes = c(genes,fields[[1]][1])
+        }
+      } else {
+        pathways.with.no.genes[k] = pathways[i,2]
+        k = k + 1
+      }
+    }
+    
+    #    write.csv(genes,'temp.genes.csv')
+    #    write.csv(z,'temp.z.csv')
+    Sys.sleep(sleep)
+    z = z + 10;
+    #print(z)
+    for (i in 1:length(submissions)) {
+      print(submissions[i])
+    }
+  }
+  
+  genes = unique(genes)
+  return(as.numeric(genes))
+  
 }
 
 #' Filter for only those genes in the data.
